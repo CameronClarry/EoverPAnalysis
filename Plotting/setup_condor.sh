02c395a2e7713e844d26d8363baab8d1aead3129
Lukas Adamek <lukas.adamek@cern.ch>
2020-10-13 18:54:20 +0200

    Include missing setup script for condor

---
+++ Plotting/setup_condor.sh
@@ -0,0 +1,7 @@
+export EOPPlottingDir=${PWD}
+export PYTHONPATH="${EOPPlottingDir}/utils:$PYTHONPATH"
+export PYTHONPATH="${EOPPlottingDir}/eop_plotting:$PYTHONPATH"
+export PYTHONPATH="${EOPPlottingDir}/macros:$PYTHONPATH"
+export X509_USER_PROXY=${EOPPlottingDir}/grid_proxy
+export PATH=/cvmfs/sft.cern.ch/lcg/external/texlive/2016/bin/x86_64-linux:$PATH
+echo "READY TO GO!"
