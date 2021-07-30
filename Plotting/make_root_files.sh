2dcb9a1cdcc48b8f28648d7f69dd4ec6c3fd5d67
Lukas Adamek <lukas.adamek@cern.ch>
2021-07-30 19:38:36 +0200

    Add script for making root files for Ota

---
+++ Plotting/make_root_files.sh
@@ -0,0 +1,2 @@
+python fit_ep_histograms.py --selection NonZeroEnergy --channels LowMuDataTightIso,PythiaJetJetTightIso
+python fit_ep_histograms.py --selection MIPSelectionHadFracAbove70 --channels LowMuData,PythiaJetJet
